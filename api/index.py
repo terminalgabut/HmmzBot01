c1cc4c7b403510793f768624d917d928485aa00f
terminalgabut <trasipahit02@gmail.com>
2025-09-08 06:43:21 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import requests
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from quiz import router as quiz_router
+from .quiz import router as quiz_router
 
 app.include_router(quiz_router)
 
