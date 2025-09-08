6949d8b9ba43d8ddc8b4d20b9b415228bc1eb54a
terminalgabut <trasipahit02@gmail.com>
2025-09-09 03:25:15 +0700

    Update supabase_client.py

--- api/supabase_client.py
+++ api/supabase_client.py
@@ -4,15 +4,12 @@ from dotenv import load_dotenv # Library untuk membaca .env saat development lok
 
 # Memuat environment variables dari file .env (hanya untuk development lokal)
 # Vercel akan mengabaikan ini dan menggunakan variabel yang diatur di dashboard.
-load_dotenv()
 
 # Ambil URL dan Key dari environment variables
-supabase_url = os.environ.get("SUPABASE_URL")
-supabase_key = os.environ.get("SUPABASE_KEY")
+supabase_url = "https://jpxtbdawajjyrvqrgijd.supabase.co"
+supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpweHRiZGF3YWpqeXJ2cXJnaWpkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjMxMjg5OCwiZXhwIjoyMDcxODg4ODk4fQ.1-CFqhW0mXPyv8jFyd-sa4m2FoCkyY4LbgiKGIKwb6I"
 
 # Cek apakah variabel ada, jika tidak, berikan error yang jelas
-if not supabase_url or not supabase_key:
-    raise ValueError("SUPABASE_URL dan SUPABASE_KEY harus diatur di environment variables.")
 
 # Inisialisasi client Supabase
 supabase: Client = create_client(supabase_url, supabase_key)
